7b81ccda44509a637091892b7b67910b733f0317
MeryAmun <chebesiawah@gmail.com>
2022-09-07 12:33:18 +0100

    video details

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react';
+import { Box,Typography } from '@mui/material';
+import { Videos  }from './';
+import { fetchFromApi } from '../utils/fetchFromApi';
+import { useParams } from 'react-router-dom';
+
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams
+
+useEffect(() => {
+ fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+ .then((data) => setVideos(data.items))
+ .catch((err) => console.log(err))
+}, [searchTerm]);
+
+
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: 'auto',
+    height:'90vh', flex:2}}>
+      <Typography variant='h4'
+      fontWeight='bold' mb={2} sx={{
+        color:'white'
+      }}>
+      Search Results for: <span style={{
+          color:'#F31503'
+        }}>
+      {searchTerm}
+        </span>
+      </Typography>
+      <Videos videos={videos}/>
+    </Box>
   )
 }
 
