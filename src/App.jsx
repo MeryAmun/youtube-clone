0f23ab34d3d7428c723ab7bf11608a959cfed340
MeryAmun <chebesiawah@gmail.com>
2022-09-07 03:19:18 +0100

    added component skeletons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { ChannelDetails,Navbar,Feed,VideoDetails, SearchFeed  } from './components';
 
 
 
@@ -6,9 +9,15 @@ const App = () => {
      
 
     return (
-        <div>
-            <h1>YouTube Clone</h1>
-        </div>
+        <Box xs={{backgroundColor: '#000'}}>
+           <Navbar/>
+            <Routes>
+                <Route path='/' exact element={<Feed/>}/>
+                <Route path='/video/:id' element={<VideoDetails/>}/>
+                <Route path='/channel/:id' element={<ChannelDetails/>}/>
+                <Route path='/search/:searchTearm' element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
     )
 }
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import ChannelDetails from "./ChannelDetails";
+import VideoDetails from "./VideoDetails";
+import SearchFeed from "./SearchFeed";
+
+export {
+    Navbar,Feed,ChannelDetails,VideoDetails,SearchFeed
+}
